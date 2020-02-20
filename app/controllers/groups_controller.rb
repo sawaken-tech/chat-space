440b249d963029c7166689ebea244573f4a89e0e
sawaken-tech <sawapaca@gmail.com>
2020-02-20 20:10:36 +0900

    init

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,6 @@
 class GroupsController < ApplicationController
 
   def index
-    @group = Group.find(params[:id])
-    @gruop.users << current_user
   end
 
   def new
@@ -26,7 +24,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
